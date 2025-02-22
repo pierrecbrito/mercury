276686ed2b5e9ee836a552c95d563a0215326f4a
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-22 00:05:21 -0300

    router for register

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from ..models.user import User
 from ..database import users_collection
 import os
+import hashlib
 
 load_dotenv()
 
@@ -12,4 +13,7 @@ user_router = APIRouter()
 async def register_user(user: User):
     if users_collection.find_one({"email": user.email}):
         raise HTTPException(status_code=400, detail="User already exists")
-    pass
+    
+    user.password_hash = hashlib.sha256(user.password_hash.encode()).hexdigest()
+    result = users_collection.insert_one(user.dict())
+    return {"message": "User registered successfully"}
