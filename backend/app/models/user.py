119272cc6f6c2e83d82836e5259ee80cc61f7295
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-21 23:43:53 -0300

    user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+class User(BaseModel):
+    email: EmailStr
+    password_hash: str
+    refresh_token: Optional[str] = None
+    created_at: datetime = datetime.utcnow()
+    updated_at: Optional[datetime] = None
