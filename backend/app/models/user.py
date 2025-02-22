b4438681bdafe8f6c3ec898388dfab6cbf38c0ad
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-22 13:27:45 -0300

    user validators for username and password

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
 from typing import Optional
 from datetime import datetime
 
@@ -8,3 +8,18 @@ class User(BaseModel):
     password_hash: str
     created_at: datetime = datetime.now()
     updated_at: Optional[datetime] = None
+
+    @field_validator('username')
+    @classmethod
+    def validate_username(cls, username):
+        if not username.strip():
+            raise ValueError("Username cannot be empty")
+        
+    @field_validator('password_hash')
+    @classmethod
+    def validate_password(cls, password):
+        if not password.strip():
+            raise ValueError("password cannot be empty")
+        
+        if len(password) < 8:
+            raise ValueError("password must be at least 8 characters long")
--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -4,11 +4,20 @@ from ..models.user import User
 from ..database import users_collection
 import os
 import hashlib
+import jwt
+from datetime import datetime, timedelta
 
 load_dotenv()
+SECRET_KEY = os.environ.get("SECRET_KEY") 
+ALGORITHM = os.environ.get("ALGORITHM")
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.now(datetime.timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 auth_router = APIRouter()
-
 @auth_router.post("/register")
 def register_user(user: User):
     if users_collection.find_one({"email": user.email}):
