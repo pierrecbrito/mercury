78e8c5881585046b40a2c50702f49950b8379e0a
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-22 10:34:24 -0300

    login route

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -17,3 +17,11 @@ async def register_user(user: User):
     user.password_hash = hashlib.sha256(user.password_hash.encode()).hexdigest()
     result = users_collection.insert_one(user.model_dump())
     return {"message": "User registered successfully"}
+
+@user_router.post("/login")
+async def login_user(user: User):
+    user.password_hash = hashlib.sha256(user.password_hash.encode()).hexdigest()
+    user = users_collection.find_one(user.model_dump())
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    return {"message": "User logged in successfully"}
