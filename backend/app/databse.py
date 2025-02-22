c1043add6ef7eb126e0b5b93875ae9debb878039
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-21 23:47:14 -0300

    database connection

---
+++ backend/app/databse.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
+mongo_url = os.environ.get("MONGO_URL", "mongodb://localhost:27017/")
+client = MongoClient(mongo_url)
+db = client["mercury"]
+users_collection = db["users"]
+messages_collection = db["messages"]
