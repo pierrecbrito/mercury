afaaac6986064fcaa60aee9d4a229025be3e8cef
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-24 21:40:12 -0300

    get conversation route

--- backend/app/routes/conversations.py
+++ backend/app/routes/conversations.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends, status
 from database import conversation_collection
 from ..models.conversation import Conversation
 from ..main import get_current_user
+from bson import ObjectId
 
 conversations_router = APIRouter(tags=["Conversations"])
 
@@ -16,8 +17,20 @@ async def get_conversations(current_user: dict = Depends(get_current_user)):
         "conversations": [parse_conversation(conversation) for conversation in conversations]
     }
 
+@conversations_router.get("/conversations/{conversation_id}")
+async def get_conversation(conversation_id: str, current_user: dict = Depends(get_current_user)):
+    user_mail = current_user['mail']
+
+    conversation = conversation_collection.find_one({"_id": ObjectId(conversation_id), "participants": user_mail})
+    if conversation is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found")
+
+    return parse_conversation(conversation)
+    
+
 def parse_conversation(conversation):
     """Helper para converter ObjectId para string"""
     conversation["id"] = str(conversation["_id"])
     del conversation["_id"]
     return conversation
+
