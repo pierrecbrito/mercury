b308717ffdc538e8fbf5f0133cab89548765521e
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-26 20:04:11 -0300

    React Router setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,12 @@ import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
   );
 }
 
