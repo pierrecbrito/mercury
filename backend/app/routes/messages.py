e6da1a39c6dc7707d9974c0ccc6647aaac9189e3
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-25 21:16:38 -0300

    Create route message

--- backend/app/routes/messages.py
+++ backend/app/routes/messages.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from bson import ObjectId
 from ..database import messages_collection
 from ..main import get_current_user
+import datetime
 
 messages_router = APIRouter(tags=["Messages"])
 
@@ -17,6 +18,13 @@ async def get_message(message_id: str, current_user: dict = Depends(get_current_
     message = messages_collection.find_one({"_id": ObjectId(message_id)})
     if message is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
+    return parse_message(message)
+    
+@messages_router.post("/messages")
+async def create_message(message: dict, current_user: dict = Depends(get_current_user)):
+    message["sent_at"] = datetime.now()
+    result = messages_collection.insert_one(message)
+    message["id"] = str(result.inserted_id)
     return parse_message(message)
 
 def parse_message(message):
