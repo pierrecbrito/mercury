58c2591f20df37bdedea33147c7e43bf1a89f9f2
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-26 19:59:02 -0300

    HomePage component

---
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './HomePage.css';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Home Page</h1>
+      <p>
+        <Link to="/login" className="login-link">Fazer Login</Link>
+      </p>
+    </div>
+  );
+}
