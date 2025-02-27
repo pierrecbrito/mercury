4e31ea5364d40dc2881b508318c28182efae68a6
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-26 23:29:52 -0300

    MessageFloat base

---
+++ frontend/src/components/MessageFloat/MessageFloat.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './MessageFloat.css';
+
+const MessageFloat: React.FC = () => {
+  return (
+    <div className="message-float">
+        
+    </div>
+  );
+}
+
+export default MessageFloat;
