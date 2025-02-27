11bde70cb0ba3e90dbff5334601a22c16022beb3
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-27 17:24:44 -0300

    MessageFloat from Andre

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import MessageFloat from "../MessageFloat/MessageFloat";
 
 const bgBanner = require('../../assets/img/bg-banner.png');
 const profile1 = require('../../assets/img/profile-2.jpg');
+const profile2 = require('../../assets/img/profile-3.jpg');
 
 const HomePage: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const HomePage: React.FC = () => {
             <h2 className="title-1">Messaging Made Simple</h2>
             <p className="subtitle-1">Seamless messaging, secure connections, and endless possibilities—all in one place.</p>
             <MessageFloat top={150} left={90} image={profile1} profileName="Tommy Maguire" message="Hello World! I'm tommy and this is a great app for contact."/>
+            <MessageFloat top={400} left={170} image={profile2} profileName="André Mendes" message="So Cool being here and enjoy."/>
         </div>
        <img src={bgBanner} alt="Banner" className="img-banner"/>
     </div>
