44316e43d95fb0a43acaeafd64fbe839f679e1c5
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-04 22:12:03 -0300

    api instance with axios

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance } from 'axios';
+
+const api: AxiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+});
+
+export default api;
