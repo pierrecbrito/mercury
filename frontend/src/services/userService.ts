41af7849580925cf1088e79ef7ee168ba05109b7
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-04 22:41:44 -0300

    login method

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,7 +1,8 @@
 import api from './api';
 import { User } from '../types/user';
+import { AuthReponse } from '../types/auth';
 
-export const login = async (email: string, password: string): Promise<User> => {
-  const response = await api.post('/auth/login', { email, password });
+export const login = async (email: string, password_hash: string): Promise<AuthReponse> => {
+  const response = await api.post('/auth/login', { email, password_hash });
   return response.data;
 }
