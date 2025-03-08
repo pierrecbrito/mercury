b2f72d984bef29e35d124eb73d78f9952aebdedf
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-07 23:17:56 -0300

    get username

--- frontend/src/components/ChatPage/ChatPage.tsx
+++ frontend/src/components/ChatPage/ChatPage.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './ChatPage.css';
+import { getUser } from "../../services/userService";
+import { MeResponse } from "../../types/auth";
 
 
-const ChatPage: React.FC<any> = () => {
+const ChatPage: React.FC =  (props) => {
+  const [user, setUser] = useState<MeResponse | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = localStorage.getItem('token') || '';
+      const userData = await getUser(token);
+      setUser(userData);
+    };
+
+    fetchUser();
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="app-container">
         <div>
-          <span></span>
+          <span>{user.username}</span>
         </div>
     </div>
   );
