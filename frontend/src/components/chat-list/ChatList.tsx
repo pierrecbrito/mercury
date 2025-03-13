5e5b941d2e281443aafc19fa3e32684b400d3775
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-03-12 22:32:48 -0300

    ChatResume base

--- frontend/src/components/chat-list/ChatList.tsx
+++ frontend/src/components/chat-list/ChatList.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './ChatList.css';
+import ChatResume from "../chat-resume/ChatResume";
 
 interface ChatListProps {}
 
 const ChatList: React.FC<ChatListProps> = () => {
   return (
     <div className="chat-list">
-       
+       <ChatResume /> 
     </div>
   );
 }
---
+++ frontend/src/components/chat-resume/ChatResume.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './ChatResume.css';
+
+interface ChatListProps {}
+
+const ChatResume: React.FC<ChatListProps> = () => {
+  return (
+    <div className="chat-resume">
+       
+    </div>
+  );
+}
+
+export default ChatResume;
